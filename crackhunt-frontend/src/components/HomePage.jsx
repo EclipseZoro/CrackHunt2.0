5d69e9d16f3d2285036f79e3900b18fe9c4c6228
AstubhMishra <astubh@gmail.com>
2025-03-25 02:15:43 +0530

    Made the dialogues for the elements

--- crackhunt-frontend/src/components/HomePage.jsx
+++ crackhunt-frontend/src/components/HomePage.jsx
@@ -9,17 +9,35 @@ import Navbar from "./Navbar.jsx";
 const HomePage = () => {
   return (
     <div className="home-container">
-      {/* Background Image */}
-      <img src={background} alt="Background" className="background-image" />
+      {/* Background Image - Added div wrapper for more control */}
+      <div className="background-wrapper">
+        <img 
+          src={background} 
+          alt="Background" 
+          className="background-image" 
+        />
+      </div>
+
       {/* Navbar */}
       <Navbar />
-      
-      
+
       {/* Branch with Pumpkins */}
-      <img src={branchPumpkins} alt="Branch with Pumpkins" className="branch-pumpkins" />
-  
+      <img 
+        src={branchPumpkins} 
+        alt="Branch with Pumpkins" 
+        className="branch-pumpkins" 
+      />
+
       {/* Charmander */}
-      <img src={charmander} alt="Charmander" className="charmander" />
+      <img 
+        src={charmander} 
+        alt="Charmander" 
+        className="charmander" 
+      />
+      <div className="logpoint">
+        Welcome to the Game Players
+      </div>
+      
     </div>
   );
 };
--- crackhunt-frontend/src/components/Leaderpage.jsx
+++ crackhunt-frontend/src/components/Leaderpage.jsx
@@ -4,20 +4,41 @@ import background from "../assets/images/leaderback2.svg";
 import board from "../assets/images/board.svg";
 import ghost1 from "../assets/images/ghost1.svg";
 import ghost2 from "../assets/images/ghost2.svg";
-
 import Navbar from "./Navbar.jsx";
 
 const HomePage = () => {
   return (
     <div className="home-container">
       {/* Background Image */}
-      <img src={background} alt="Background" className="background-image" />
+      <div className="background-wrapper">
+        <img 
+          src={background} 
+          alt="Background" 
+          className="background-image" 
+        />
+      </div>
+      
       {/* Navbar */}
       <Navbar />
-
+      
+      {/* Board */}
       <img src={board} alt="board" className="board" />
-      <img src={ghost1} alt="board" className="ghost1" />
-      <img src={ghost2} alt="board" className="ghost2" />
+      
+      {/* Ghost 1 with Tooltip */}
+      <div className="ghost1-container">
+        <img src={ghost1} alt="Ghost 1" className="ghost1" />
+        <div className="ghost1-tooltip">
+          Climb the ladder or go to grave
+        </div>
+      </div>
+      
+      {/* Ghost 2 with Tooltip */}
+      <div className="ghost2-container">
+        <img src={ghost2} alt="Ghost 2" className="ghost2" />
+        <div className="ghost2-tooltip">
+          Welcome to the Leaderboard
+        </div>
+      </div>
     </div>
   );
 };
